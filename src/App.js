dadf45bfe4894e51964842e82a8c320adf858642
Moncivais Maciel David <davidmoncivais@live.com.mx>
2025-05-02 15:02:42 -0700

    agregar logo

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ import routes from "routes";
 import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
 
 // Images
-import brandWhite from "assets/images/logo-ct.png";
-import brandDark from "assets/images/logo-ct-dark.png";
+import brandWhite from "assets/images/Logo-nc-1.png";
+import brandDark from "assets/images/Logo-nc-1.png";
 
 export default function App() {
   const [controller, dispatch] = useMaterialUIController();
